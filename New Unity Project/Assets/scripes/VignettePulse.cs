fe3aef191396014152c64356d02b5fc59e1f783f
Unknown <wolfking068@gmail.com>
2018-09-11 17:11:25 -0700

    yey

--- New Unity Project/Assets/scripes/VignettePulse.cs
+++ New Unity Project/Assets/scripes/VignettePulse.cs
@@ -3,21 +3,34 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Rendering.PostProcessing;
 public class VignettePulse : MonoBehaviour {
-    PostProcessVolume m_Volume;
-    Vignette m_Vignette;
-
+    public PostProcessVolume m_Volume;
+    public Vignette m_Vignette;
+    public float health = 100;
     void Start()
     {
         m_Vignette = ScriptableObject.CreateInstance<Vignette>();
-        m_Vignette.enabled.Override(true);
-        m_Vignette.intensity.Override(1f);
+        ////m_Vignette.enabled.Override(true);
+        ////m_Vignette.intensity.Override(1f);
 
-        m_Volume = PostProcessManager.instance.QuickVolume(gameObject.layer, 100f, m_Vignette);
+        ////m_Volume = PostProcessManager.instance.QuickVolume(gameObject.layer, 100f, m_Vignette);
     }
 
     void Update()
     {
-        m_Vignette.intensity.value = Mathf.Sin(Time.realtimeSinceStartup);
+        if(health <= 50)
+        {
+            m_Vignette.intensity.value = Mathf.Sin(Time.realtimeSinceStartup);
+            m_Vignette = ScriptableObject.CreateInstance<Vignette>();
+            m_Vignette.enabled.Override(true);
+            m_Vignette.intensity.Override(1f);
+
+            m_Volume = PostProcessManager.instance.QuickVolume(gameObject.layer, 100f, m_Vignette);
+        }
+        if(health >= 60)
+        {
+            m_Vignette.enabled.Override(false);
+        }
+        //m_Vignette.intensity.value = Mathf.Sin(Time.realtimeSinceStartup);
     }
 
     void OnDestroy()
